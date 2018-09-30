010700fb0155ea31917e692320c4928a3219dd26
Peter Osterlund <peterosterlund2@gmail.com>
2018-09-30 13:06:45 +0200

    Update default syzygy path.

--- src/wdlcomp.cpp
+++ src/wdlcomp.cpp
@@ -17,7 +17,8 @@ setupTB() {
     UciParams::rtbPath->set("/home/petero/chess/rtb/wdl:"
                             "/home/petero/chess/rtb/dtz:"
                             "/home/petero/chess/rtb/6wdl:"
-                            "/home/petero/chess/rtb/6dtz");
+                            "/home/petero/chess/rtb/6dtz:"
+                            "/home/petero/chess/rtb/7men");
 }
 
 static void
